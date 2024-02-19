59c99762b0c8fe37cab0720adb31961e922b0e1d
Antonio Ojea <aojea@google.com>
2024-02-19 16:20:05 +0000

    fix build ebpf script

--- hack/build-ebpf.sh
+++ hack/build-ebpf.sh
@@ -7,5 +7,4 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 cd "${REPO_ROOT}"
 
 # bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
-clang -target bpf -Wall -O2 -I. -c bpf/nat64.c -o bpf/nat64.o
 clang -target bpf -g -Wall -O2 -I. -c bpf/nat64.c -o bpf/nat64.o
